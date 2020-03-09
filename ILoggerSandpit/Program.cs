79387abe166a5fed9ca2c3e00acaec7076340882
Adrian <adrian.cooke@mitrefinch.co.uk>
2020-03-09 14:14:58 +0000

    Add project files.

---
+++ ILoggerSandpit/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using Kernel.CrossCuttingConcerns;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace ILoggerSandpit
+{
+    public class Program : SerilogAspNetCoreWebAppBaseProgram
+    {
+        public static void Main(string[] args)
+        {
+            //GetConfiguration.  Move to Kernel...  Perhaps?
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{environment}.json", false);
+
+            if (environment == EnvironmentName.Development)
+            {
+                try
+                {
+                    builder.AddUserSecrets<Startup>();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+
+            builder.AddEnvironmentVariables();
+            var configuration = builder.Build();
+            //Move to Kernel..  To get it as var configuration = GetConfiguration.
+
+            SerilogMainBootstrapper(() => CreateWebHostBuilder(args).Build().Run(), configuration);
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) => 
+            BuildWebHostUsingSerilog(WebHost.CreateDefaultBuilder(args).UseStartup<Startup>(), args);
+    }
+}
---
+++ Kernel.CrossCuttingConcerns/SerilogAspNetCoreWebAppBaseProgram.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+
+namespace Kernel.CrossCuttingConcerns
+{
+    public abstract class SerilogAspNetCoreWebAppBaseProgram
+    {
+        public static IWebHostBuilder BuildWebHostUsingSerilog(IWebHostBuilder webHostBuilder, string[] args) => webHostBuilder.UseSerilog();
+
+        public static int SerilogMainBootstrapper(Action run, IConfiguration configuration)
+        {
+            //var configuration = new ConfigurationBuilder()
+            //    .SetBasePath(Directory.GetCurrentDirectory())
+            //    .AddJsonFile("appsettings.json")
+            //    .AddJsonFile($"appsettings.{environment}.json", false);
+
+            //if (environment == EnvironmentName.Development)
+            //{
+            //    try
+            //    {
+            //        builder.AddUserSecrets<Startup>();
+            //    }
+            //    catch (Exception e)
+            //    {
+            //        Console.WriteLine(e);
+            //    }
+            //}
+
+            //builder.AddEnvironmentVariables();
+            //var configuration = builder.Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                //.Enrich.FromLogContext()
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting web host");
+                run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        //public static int MainWrapper(Action run)
+        //{
+        //    var configuration = new ConfigurationBuilder()
+        //        .SetBasePath(Directory.GetCurrentDirectory())
+        //        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        //        .Build();
+
+        //    Log.Logger = new LoggerConfiguration()
+        //        .ReadFrom.Configuration(configuration)
+        //        .Enrich.FromLogContext()
+        //        .CreateLogger();
+
+        //    try
+        //    {
+        //        Log.Information("Starting web host");
+        //        run();
+        //        return 0;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Log.Fatal(ex, "Host terminated unexpectedly");
+        //        return 1;
+        //    }
+        //    finally
+        //    {
+        //        Log.CloseAndFlush();
+        //    }
+        //}
+    }
+}
