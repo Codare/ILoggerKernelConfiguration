b7a8bde960c7c3e54fa6fadbeeed8e8a4614a695
Adrian <adrian.cooke@mitrefinch.co.uk>
2020-03-13 23:18:06 +0000

    wip

---
+++ Kernel.CrossCuttingConcerns/ILoggerExtensions/LoggerExtensionDebug.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+
+namespace Kernel.CrossCuttingConcerns.ILoggerExtensions
+{
+    public static class LoggerExtensionsDebug
+    {
+        /// <summary>Writes a debug log message.</summary>
+        /// <param name="logger">The <see cref="T:Microsoft.Extensions.Logging.ILogger" /> to write to.</param>
+        /// <param name="triggerAlert">Boolean parameter to indicate that an alert is to be triggered.</param>
+        /// <param name="message">Format string of the log message in message template format. Example: <code>"User logged in from"</code></param>
+        /// <example>logger.LogDebug(0, exception, "Error while processing request", true|false)</example>
+        public static void LogDebug(this ILogger logger, string message, bool triggerAlert)
+        {
+            LogDebugImpl(logger, message, triggerAlert);
+        }
+
+        /// <summary>
+        /// Formats and writes a debug log message.
+        /// </summary>
+        /// <param name="logger">The <see cref="T:Microsoft.Extensions.Logging.ILogger" /> to write to.</param>
+        /// <param name="message">Format string of the log message in message template format. Example: <code>"User logged in from"</code></param>
+        /// <param name="triggerAlert">Boolean parameter to indicate that an alert is to be triggered.</param>
+        /// <param name="args">An object array that contains zero or more objects to format.</param>
+        public static void LogDebug(this ILogger logger, string message, bool triggerAlert, params object[] args)
+        {
+            LogDebugImpl(logger, message, triggerAlert, null, args);
+        }
+
+        /// <summary>Writes a debug log message.</summary>
+        /// <param name="logger">The <see cref="T:Microsoft.Extensions.Logging.ILogger" /> to write to.</param>
+        /// <param name="exception">The exception to log.</param>
+        /// <param name="message">Format string of the log message in message template format. Example: <code>"User logged in from"</code></param>
+        /// <param name="triggerAlert">Boolean parameter to indicate that an alert is to be triggered.</param>
+        /// <example>logger.LogDebug(0, exception, "Error while processing request", true|false)</example>
+        public static void LogDebug(this ILogger logger, Exception exception, string message, bool triggerAlert)
+        {
+            LogDebugImpl(logger, message, triggerAlert, exception);
+        }
+
+        /// <summary>Formats and writes a debug log message.</summary>
+        /// <param name="logger">The <see cref="T:Microsoft.Extensions.Logging.ILogger" /> to write to.</param>
+        /// <param name="triggerAlert">Boolean parameter to indicate that an alert is to be triggered.</param>
+        /// <param name="exception">The exception to log.</param>
+        /// <param name="message">Format string of the log message in message template format. Example: <code>"User {User} logged in from {Address}"</code></param>
+        /// <param name="args">An object array that contains zero or more objects to format.</param>
+        /// <example>logger.LogDebug(0, exception, "Error while processing request from {Address}", address, true|false)</example>
+        public static void LogDebug(this ILogger logger, Exception exception, string message, bool triggerAlert, params object[] args)
+        {
+            LogDebugImpl(logger, message, triggerAlert, exception, args);
+        }
+
+        /// <summary>Formats and writes a debug log message.</summary>
+        /// <param name="logger">The <see cref="T:Microsoft.Extensions.Logging.ILogger" /> to write to.</param>
+        /// <param name="triggerAlert">Boolean parameter to indicate that an alert is to be triggered.</param>
+        /// <param name="exception">The exception to log.</param>
+        /// <param name="message">Format string of the log message in message template format. Example: <code>"User {User} logged in from {Address}"</code></param>
+        /// <param name="args">An object array that contains zero or more objects to format.</param>
+        /// <example>logger.LogDebug(0, "Error while processing request from {Address}", true|false, exception, address)</example>
+        private static void LogDebugImpl(this ILogger logger, string message, bool triggerAlert, Exception exception = null, params object[] args)
+        {
+            Dictionary<string, object> state = LoggerMessageHelper.GetLogMessageState(triggerAlert, message, args);
+
+            var expandoObject = new ExpandoObject();
+            var keyValuePairs = (ICollection<KeyValuePair<string, object>>)expandoObject;
+
+            foreach (var keyValuePair in state)
+                keyValuePairs.Add(keyValuePair);
+
+            dynamic eoDynamic = expandoObject;
+
+            logger.Log(logLevel: LogLevel.Debug, eventId: new EventId(9999), state: eoDynamic, exception: exception, formatter: null);
+        }
+
+
+    }
+}
---
+++ Kernel.CrossCuttingConcerns/ILoggerExtensions/LoggerExtensionsError.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+
+namespace Kernel.CrossCuttingConcerns.ILoggerExtensions
+{
+    public static class LoggerExtensionsError
+    {
+        /// <summary>Writes an error log message.</summary>
+        /// <param name="logger">The <see cref="T:Microsoft.Extensions.Logging.ILogger" /> to write to.</param>
+        /// <param name="triggerAlert">Boolean parameter to indicate that an alert is to be triggered.</param>
+        /// <param name="message">Format string of the log message in message template format. Example: <code>"User logged in from"</code></param>
+        /// <example>logger.LogError(0, exception, "Error while processing request", true|false)</example>
+        public static void LogError(this ILogger logger, string message, bool triggerAlert)
+        {
+            LogErrorImpl(logger, message, triggerAlert);
+        }
+
+        /// <summary>
+        /// Formats and writes an error log message.
+        /// </summary>
+        /// <param name="logger">The <see cref="T:Microsoft.Extensions.Logging.ILogger" /> to write to.</param>
+        /// <param name="message">Format string of the log message in message template format. Example: <code>"User logged in from"</code></param>
+        /// <param name="triggerAlert">Boolean parameter to indicate that an alert is to be triggered.</param>
+        /// <param name="args">An object array that contains zero or more objects to format.</param>
+        public static void LogError(this ILogger logger, string message, bool triggerAlert, params object[] args)
+        {
+            LogErrorImpl(logger, message, triggerAlert, null, args);
+        }
+
+        /// <summary>Writes an error log message.</summary>
+        /// <param name="logger">The <see cref="T:Microsoft.Extensions.Logging.ILogger" /> to write to.</param>
+        /// <param name="exception">The exception to log.</param>
+        /// <param name="message">Format string of the log message in message template format. Example: <code>"User logged in from"</code></param>
+        /// <param name="triggerAlert">Boolean parameter to indicate that an alert is to be triggered.</param>
+        /// <example>logger.LogError(0, exception, "Error while processing request", true|false)</example>
+        public static void LogError(this ILogger logger, Exception exception, string message, bool triggerAlert)
+        {
+            LogErrorImpl(logger, message, triggerAlert, exception);
+        }
+
+        /// <summary>Formats and writes an error log message.</summary>
+        /// <param name="logger">The <see cref="T:Microsoft.Extensions.Logging.ILogger" /> to write to.</param>
+        /// <param name="triggerAlert">Boolean parameter to indicate that an alert is to be triggered.</param>
+        /// <param name="exception">The exception to log.</param>
+        /// <param name="message">Format string of the log message in message template format. Example: <code>"User {User} logged in from {Address}"</code></param>
+        /// <param name="args">An object array that contains zero or more objects to format.</param>
+        /// <example>logger.LogError(0, exception, "Error while processing request from {Address}", address, true|false)</example>
+        public static void LogError(this ILogger logger, Exception exception, string message, bool triggerAlert, params object[] args)
+        {
+            LogErrorImpl(logger, message, triggerAlert, exception, args);
+        }
+
+        /// <summary>Formats and writes an error log message.</summary>
+        /// <param name="logger">The <see cref="T:Microsoft.Extensions.Logging.ILogger" /> to write to.</param>
+        /// <param name="triggerAlert">Boolean parameter to indicate that an alert is to be triggered.</param>
+        /// <param name="exception">The exception to log.</param>
+        /// <param name="message">Format string of the log message in message template format. Example: <code>"User {User} logged in from {Address}"</code></param>
+        /// <param name="args">An object array that contains zero or more objects to format.</param>
+        /// <example>logger.LogError(0, "Error while processing request from {Address}", true|false, exception, address)</example>
+        private static void LogErrorImpl(this ILogger logger, string message, bool triggerAlert, Exception exception = null, params object[] args)
+        {
+            Dictionary<string, object> state = LoggerMessageHelper.GetLogMessageState(triggerAlert, message, args);
+
+            var expandoObject = new ExpandoObject();
+            var keyValuePairs = (ICollection<KeyValuePair<string, object>>)expandoObject;
+
+            foreach (var keyValuePair in state)
+                keyValuePairs.Add(keyValuePair);
+
+            dynamic eoDynamic = expandoObject;
+
+            logger.Log(logLevel: LogLevel.Error, eventId: new EventId(9999), state: eoDynamic, exception: exception, formatter: null);
+        }
+
+
+    }
+}
